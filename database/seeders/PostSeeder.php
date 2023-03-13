ef9ea76916ee7bd3e42ad1b7ece1ca263db17bc1
aldinpramudya <aldinarielpramudya@gmail.com>
2023-03-13 10:11:21 +0700

    add seeding post

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PostSeeder extends Seeder
 {
@@ -14,6 +15,8 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('posts')->insert([
+            
+        ]);
     }
 }
