4e679befe4049cb6026197a05b64994002dffdc1
anhar <fndedek@gmailcom>
2023-03-14 01:28:29 +0700

    update-profile-kholid

--- resources/views/Kholid/profileKholid.blade.php
+++ resources/views/Kholid/profileKholid.blade.php
@@ -4,30 +4,30 @@
 @parent
 @endsection
 @section('kholidprofile')
-<h3 class="profile-username text-center">Kholid Maulidi</h3>
+<h3 class="profile-username text-center">{{$post->name}}</h3>
 <div class="text-center">
   <img class="profile-user-img img-fluid img-circle"
       src="../../dist/img/kholid-crop.jpeg"
       alt="User profile picture">
 </div>
 
-                <p class="text-muted text-center">Mahasiswa</p>
+  <p class="text-muted text-center">{{$post->role}}</p>
 
-                <ul class="list-group list-group-unbordered mb-3">
-                  <li class="list-group-item">
-                    <b>TTL</b> <a class="float-right">Sumenep, 08 Mei 2003</a>
-                  </li>
-                  <li class="list-group-item">
-                    <b>Alamat</b> <a class="float-right">Sumenep</a>
-                  </li>
-                  <li class="list-group-item">
-                    <b>Email</b> <a class="float-right">kholidmaulidi85@gmail.com</a>
-                  </li>
-                </ul>
+  <ul class="list-group list-group-unbordered mb-3">
+      <li class="list-group-item">
+        <b>TTL</b> <a class="float-right">Sumenep, 08 Mei 2003</a>
+      </li>
+      <li class="list-group-item">
+        <b>Alamat</b> <a class="float-right">{{$post->address}}</a>
+      </li>
+      <li class="list-group-item">
+        <b>Email</b> <a class="float-right">{{$post->phone}}</a>
+      </li>
+  </ul>
 
-                <a href="#" class="btn btn-primary btn-block"><b>View More</b></a>
-              </div>
+  <a href="#" class="btn btn-primary btn-block"><b>View More</b></a>
+</div>
               <!-- /.card-body -->
-            </div>
+</div>
             <!-- /.card -->
 @endsection
