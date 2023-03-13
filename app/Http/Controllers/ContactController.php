ac6d1dd1f0395e7e2e6fac8b232329d201978e3a
aldinpramudya <aldinarielpramudya@gmail.com>
2023-03-13 09:11:12 +0700

    add meet 4 from meet 3 prak2

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function profile($id=0){
+        if($id == 0){
+            return view('content.contact');
+        } else if($id == 1){  
+            return view('aldin.profile');
+        } else if($id == 2){
+            return view('Kholid.profileKholid');
+        } else{
+            return 'error';
+        }
+    }
+}
